c64f000a4b02cd3fa86e613570ae7b7a01da7b68
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2020-06-25 12:03:29 -0700

    Connecting State to Duild Controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,26 @@
 import React from "react";
 import classes from "./BuildControls.module.css";
+import BuiltControl from "./BuildControl./BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
 
 const BuildControls = (props) => {
-  return <div></div>;
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map((ctrl) => (
+        <BuiltControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BuildControls;
