80d08582aad57ac2159e79d1cae750dfe6209843
YOUR_GITHUB_USERNAME <sunem.family@gmail.com>
2020-06-27 14:17:56 -0700

    creating a responsive sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => (
   <Aux>
